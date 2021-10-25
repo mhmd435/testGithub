cb39c7849a0eb31e0b0355288d1ffeb9e7c3c28a
mhmd aghajani <mhm.aghajani435@gmail.com>
2021-10-25 21:22:30 +0330

    first commit

--- app/src/main/java/com/besenior/githubtestapp/MainActivity.java
+++ app/src/main/java/com/besenior/githubtestapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.besenior.githubtestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
